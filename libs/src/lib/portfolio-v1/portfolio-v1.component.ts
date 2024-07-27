e5b0be939fe5a2d0e045c1e10a41f9034e08b8c3
vishnukumar <vishnu.kumar@digio.in>
2024-07-27 11:00:16 +0530

    created sidebar and menu and component design

--- libs/src/lib/portfolio-v1/portfolio-v1.component.ts
+++ libs/src/lib/portfolio-v1/portfolio-v1.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {InfoSidebarComponent} from "./components/info-sidebar/info-sidebar.component";
 
 @Component({
   selector: 'vi-portfolio-v1',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InfoSidebarComponent],
   templateUrl: './portfolio-v1.component.html',
   styleUrl: './portfolio-v1.component.scss',
 })
